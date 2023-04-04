6e5fbf3a6f02fc435c319567a6583b83eaffe8b2
Vinay Bhat <vinaybhathg11@gmail.com>
2023-04-04 22:33:33 +0530

    header
    designed header for youtube

--- my-youtube/src/components/Head.js
+++ my-youtube/src/components/Head.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Head = () => {
-  return <div>Head</div>;
+  return (
+    <div className="grid grid-flow-col">
+      <div className="flex space-x-3">
+        <img className="h-10 w-10" alt="menu" src="menu.png" />
+
+        <img className="h-15 w-30" alt="Home" src="youtube.png" />
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div>
+        <img alt="user" src="user.png" />
+      </div>
+    </div>
+  );
 };
 
 export default Head;
