d0051c7dfcc15baa3390e1bcad7ef5a3e4ab24e2
Vinay Bhat <vinaybhathg11@gmail.com>
2023-05-11 20:32:08 +0530

    feat: comments, live-chat
    added features
    
    - nested comments
    - live-chat

---
+++ my-youtube/src/components/LiveChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LiveChat = () => {
+  return (
+    <div className="w-full h-[400px] ml-2 p-2 border border-gray-500 bg-gray-100 rounded-md">
+      LiveChat
+    </div>
+  );
+};
+
+export default LiveChat;
